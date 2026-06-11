9b120a729465ac6e0ba8178317f73af693302ff4
TKriel_ST10467193 <ST10467193@myemeris.edu.za>
2026-06-12 00:07:56 +0200

    Initial Commit

---
+++ RxLog/RxLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  RxLogApp.swift
+//  RxLog
+//
+//  Created by Tristan Kriel on 2026/06/12.
+//
+
+import SwiftUI
+
+@main
+struct RxLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
